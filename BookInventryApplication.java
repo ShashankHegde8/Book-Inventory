ee6f52e04eace2f071ef11183d04fc18dba55f9c
Shashank Hegde <86239282+ShashankHegde8@users.noreply.github.com>
2024-08-02 23:57:00 +0530

    Add files via upload

---
+++ BookInventryApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_inventry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookInventryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookInventryApplication.class, args);
+	}
+
+}
